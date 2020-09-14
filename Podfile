2a8215293766b6590552b5f0f11259aae12f8a8e
LM Bibera <lyndon.bibera@headhuntr.io>
2020-09-14 19:07:49 +0800

    Assets, CocoaPods, Initial elements

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '13.0'
+
+target 'WikiBrowser' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for WikiBrowser
+  pod 'Alamofire', '~> 5.2'
+  pod 'SwiftyJSON', '~> 4.0'
+  pod 'SDWebImage', '~> 5.0'
+
+end
--- WikiBrowser/ViewController.swift
+++ WikiBrowser/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        
+        searchField.text = ""
+    }
+    
 }
 
