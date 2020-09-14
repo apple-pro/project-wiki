d3fff551f1b2baf0e9080a84bc0b63463e7fe371
LM Bibera <lyndon.bibera@headhuntr.io>
2020-09-14 20:21:48 +0800

    Display text result

--- WikiBrowser/ViewController.swift
+++ WikiBrowser/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     let wikipediaURL = "https://en.wikipedia.org/w/api.php"
 
     @IBOutlet weak var searchField: UITextField!
+    @IBOutlet weak var articleImage: UIImageView!
+    @IBOutlet weak var articleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,7 +37,22 @@ class ViewController: UIViewController {
         ]
         
         AF.request(wikipediaURL, method: HTTPMethod.get, parameters: parameters).responseJSON { response in
-            print(response)
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                
+                let firstPage = json["query"]["pageids"][0].stringValue
+                let firstResult = json["query"]["pages"][firstPage]
+                
+                print("Title: \(firstResult["extract"])")
+                print("Extract: \(firstResult["extract"])")
+                
+                DispatchQueue.main.async {
+                    self.articleLabel.text = firstResult["extract"].stringValue
+                }
+            case .failure(let error):
+                print(error)
+            }
         }
         
         searchField.text = ""
